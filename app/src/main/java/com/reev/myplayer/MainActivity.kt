d8fdc4a0dd0600ef92f42e370e7f889127b7e5b1
heireev <muhammadrifqi216@gmail.com>
2023-05-04 15:22:53 +0800

    Latihan Exoplayer

--- app/src/main/java/com/reev/myplayer/MainActivity.kt
+++ app/src/main/java/com/reev/myplayer/MainActivity.kt
@@ -2,10 +2,28 @@ package com.reev.myplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.reev.myplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+    }
+
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
+
+        val player = ExoPlayer.Builder(this).build()
+        viewBinding.videoView.player = player
+
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        player.setMediaItem(mediaItem)
+        player.prepare()
     }
 }
